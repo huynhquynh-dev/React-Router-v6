907535f38da645ddefc007212d542499869f6dac
huynhquynh-dev <huynhquynh-dev>
2022-02-26 14:00:17 +0700

    -Add data sample

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,19 @@
+import { abouts } from 'data/abouts'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
         <main>
             <h2>About</h2>
-        </main>
+            <div>
+                <nav>
+                    {abouts.map(about => (
+                        <Link key={about.id} to={`${about.id}`} > {about.name}</Link>
+                    ))}
+                </nav>
+            </div>
+        </main >
     )
 }
 
---
+++ src/data/abouts.ts
@@ -0,0 +1,15 @@
+interface About {
+    id: number;
+    name: string;
+}
+
+export const abouts: About[] = [
+    {
+        id: 1,
+        name: 'Google',
+    },
+    {
+        id: 2,
+        name: 'Facebook',
+    },
+];
