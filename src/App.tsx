79c3b9fd415475236e4ce9c505636a5f31720a9d
huynhquynh-dev <huynhquynh-dev>
2022-02-26 13:50:18 +0700

    -Add router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       <nav>
         <Link to="/home">Home</Link> | <Link to="/about">About</Link>
       </nav>
+      <Outlet /> {/* Giống như {children}, render các path con*/}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import { screen } from '@testing-library/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
